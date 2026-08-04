2b13d6cd964abad1adf91bdef4d4a27eace5bd47
Clinton Selke <clinuxrulz@gmail.com>
2026-08-04 16:28:04 +1000

    reduced bundle size by tweaking fontawesome imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
-import "@fortawesome/fontawesome-free/js/all";
-import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
+import "@fortawesome/fontawesome-free/css/solid.min.css";
 import { render } from "@solidjs/web";
 import App from "./App";
 
