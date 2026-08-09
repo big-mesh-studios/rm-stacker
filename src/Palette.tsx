a952e3811a528459d96be35200eb19038e9e889c
bigmistqke <vandijckv@gmail.com>
2026-08-09 16:06:29 +0200

    feat: scroll selected colour into view in Palette

--- src/Palette.tsx
+++ src/Palette.tsx
@@ -1,4 +1,4 @@
-import { Component } from "solid-js";
+import { Component, createEffect, createMemo, createRoot, For } from "solid-js";
 import { RGBA } from "./types";
 import { areRGBAsEqual } from "./utils";
 
@@ -9,30 +9,46 @@ const Palette: Component<{
 }> = props => {
   return (
     <div style="height: 100%; overflow-y: scroll;">
-      {props.palette.map(colour => (
-        <div
-          style={{
-            border:
-              props.selectedColour && areRGBAsEqual(colour, props.selectedColour)
-                ? "4px solid green"
-                : undefined,
-            padding:
-              props.selectedColour === undefined || !areRGBAsEqual(colour, props.selectedColour)
-                ? "4px"
-                : undefined,
-          }}
-        >
-          <div
-            class="size-[32px]"
-            style={{
-              "background-color": `rgba(${colour.r}, ${colour.g}, ${colour.b}, ${colour.a})`,
-            }}
-            onClick={() => {
-              props.onSelect(colour);
-            }}
-          />
-        </div>
-      ))}
+      <For each={props.palette}>
+        {colour => {
+          const isActive = createMemo(
+            () => props.selectedColour && areRGBAsEqual(colour, props.selectedColour),
+          );
+
+          return (
+            <div
+              ref={element =>
+                createRoot(() => {
+                  createEffect(isActive, active => {
+                    if (!active) {
+                      return;
+                    }
+                    element.scrollIntoView({
+                      block: "start",
+                      inline: "start",
+                      behavior: "instant",
+                    });
+                  });
+                })
+              }
+              style={{
+                border: isActive() ? "4px solid green" : undefined,
+                padding: !isActive() ? "4px" : undefined,
+              }}
+            >
+              <div
+                class="size-[32px]"
+                style={{
+                  "background-color": `rgba(${colour.r}, ${colour.g}, ${colour.b}, ${colour.a})`,
+                }}
+                onClick={() => {
+                  props.onSelect(colour);
+                }}
+              />
+            </div>
+          );
+        }}
+      </For>
     </div>
   );
 };
